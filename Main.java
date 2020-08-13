7ffeee0b279d0d8ffe3966f9190533e67ed07ea5
zl81906 <69637647+zl81906@users.noreply.github.com>
2020-08-13 11:02:45 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My name is Zhihao Liu.");
+		System.out.println("I like to play video games, badminton, and table tennis.");
+		System.out.println("I want to go to UC Berkeley when I graduate from high school.");
+		System.out.println("I want to major in computer science.");
+		System.out.println("In the future, I want to create my own video games for people to play, and let people enjoy them!");
+		System.out.println("I will take this class serious, and do my best to learn about coding.");
+	}
+}
